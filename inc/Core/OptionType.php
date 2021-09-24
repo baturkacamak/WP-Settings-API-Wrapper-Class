5108a2d6e024b5edc69a7340b4c60c1c7e41eaa2
Batur <batisonline@gmail.com>
2021-09-24 16:19:46 +0200

    added before and after render

--- inc/Core/OptionType.php
+++ inc/Core/OptionType.php
@@ -30,14 +30,41 @@ abstract class OptionType
     protected function prepare(&$options)
     {
         $options['atts']['name'] = $options['id'];
+        // get default value
+        $default = ml_atv('default', $options, '');
+        // if no input value, use default
+        $options['atts']['value'] = ml_atv('value', $options, $default, true);
     }
+
+    protected function hasValue($value)
+    {
+        return !empty($value);
+    }
+
+    protected function renderBefore($options)
+    {
+        return '';
+    }
+
+    protected function renderAfter($options)
+    {
+        if (isset($options['desc'])) {
+            return "<div>{$options['desc']}</div>";
+        }
+
+        return '';
+    }
+
 
     public function render($options)
     {
         $this->prepare($options);
 
+        $before        = $this->renderBefore($options);
+        $element_input = $this->renderItem($options);
+        $after         = $this->renderAfter($options);
 
-        return $this->renderItem($options);
+        return $before . $element_input . $after;
     }
 
 }
