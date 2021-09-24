c3e9240f5ee9eeb65a84d4404f32e6cd88b2d9f4
Batur <batisonline@gmail.com>
2021-09-24 15:52:31 +0200

    replaced type assigning

--- inc/OptionTypes/Text.php
+++ inc/OptionTypes/Text.php
@@ -6,18 +6,19 @@ namespace Manalard\OptionTypes;
 
 use Manalard\Core\OptionType;
 
+
 if (!class_exists('Manalard\OptionTypes\Text')) {
     class Text extends OptionType
     {
 
         public function getType()
         {
-            return 'input';
+            return 'text';
         }
 
         protected function renderItem($options)
         {
-            $option['atts']['type'] = 'text';
+            $options['atts']['type'] = ml_atv('atts/type', $options, $this->getType());
 
             return "<input {$this->attsToHtml($options['atts'])} />";
         }
